31d3dd435ffd737cb4fb0438e9724dd7551f883e
RuchitGohel <141817181+RuchitGohel@users.noreply.github.com>
2024-08-08 09:08:13 +0530

    Add files via upload

---
+++ RUCHIT_7212_JAVA/P_1/hello.java
@@ -0,0 +1,11 @@
+/*
+Program - 1 : Write a program to print "hello world!"
+*/
+
+class hello
+{
+ public static void main(String args[])
+	{
+           System.out.println("Hello world!");
+	}
+}
---
+++ RUCHIT_7212_JAVA/P_2/Intro.java
@@ -0,0 +1,17 @@
+/*
+Program-2 : Prepare your intro in Java
+*/
+
+class intro
+{
+ public static void main(String args[])
+	{
+              System.out.println("Hello Everyone!");
+	      System.out.println("Myself Ruchit.");
+              System.out.println("I am in Marwadi University");
+              System.out.println("Pursuing BCA");
+              System.out.println("Currently in my 2nd Year");
+              System.out.println("I live in Rajkot");
+              System.out.println("I use university transportation everyday."); 
+	}
+}
