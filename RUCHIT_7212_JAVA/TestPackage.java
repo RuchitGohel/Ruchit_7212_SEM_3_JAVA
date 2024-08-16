f0beae5f11e0b07f4742e09d9787ca72dc07dead
RuchitGohel <141817181+RuchitGohel@users.noreply.github.com>
2024-08-16 12:19:51 +0530

    Add files via upload

---
+++ RUCHIT_7212_JAVA/TestPackage.java
@@ -0,0 +1,9 @@
+import mypackage.MyClass;
+
+public class TestPackage
+{
+	public static void main(String[] args){
+		MyClass obj = new MyClass();
+		obj.displayMsg();
+	}
+}
---
+++ RUCHIT_7212_JAVA/mypackage/MyClass.java
@@ -0,0 +1,11 @@
+//step - 1 
+
+package mypackage;
+
+public class MyClass
+{
+	public void displayMsg()
+	{
+		System.out.println("Hello from MyClass in mypackage.");
+	}
+}
